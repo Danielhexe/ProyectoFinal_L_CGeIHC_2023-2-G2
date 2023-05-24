c476cd037bb46845624f9d49a55118bfc0aa467b
DanielHexe <CABALLERO.JUAN.3B@gmail.com>
2023-05-24 13:33:38 -0600

    Ejecución del código | Faltan texturas quitada

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -400,6 +400,8 @@ int main()
 	spotLightCount++;
 
 
+	//Verificacion
+
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
 		uniformSpecularIntensity = 0, uniformShininess = 0, uniformTextureOffset=0;
 	GLuint uniformColor = 0;
