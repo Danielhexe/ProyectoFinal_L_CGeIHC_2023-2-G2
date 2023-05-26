b7f9c9a7548cdf829741f61ff17827fe0fa2327f
AlexisDanielCote <alexis_cote@comunidad.unam.mx>
2023-05-25 21:24:40 -0600

    Comentario Silla

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -475,13 +475,13 @@ int main()
 		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		meshList[3]->RenderMesh();
 
-		/* Se prueba cargar la silla */
+		/*/* Se prueba cargar la silla 
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(0.0f, 3.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 		model = glm::rotate(model, 0 * toRadians, glm::vec3(1.0f, 0.0f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		silla.RenderModel();
+		silla.RenderModel();*/
 		
 
 		/* ------------------------- Textura con movimiento------------------------*/
