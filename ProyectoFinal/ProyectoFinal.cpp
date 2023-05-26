7d1829f0cfcb5fdd2fa512ded7f20ee4bfbab214
DanielHexe <CABALLERO.JUAN.3B@gmail.com>
2023-05-26 11:12:52 -0600

    Se carga el modelo texturizado del restaurante

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -59,6 +59,7 @@ Model Kunai;
 Model Kama;
 Model Kubikiribocho;
 Model Shuriken4Dagas;
+Model Restaurante;
 
 //materiales
 Material Material_brillante;
@@ -321,6 +322,8 @@ int main()
 	Kubikiribocho.LoadModel("Models/Kubikiribocho.obj");
 	Shuriken4Dagas = Model();
 	Shuriken4Dagas.LoadModel("Models/Shuriken4Dagas.obj");
+	Restaurante = Model();
+	Restaurante.LoadModel("Models/RestauranteCompleto.obj");
 
 
 	/* Variable a utilizar */
@@ -558,15 +561,12 @@ int main()
 		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Shuriken4Dagas.RenderModel();
-
-
-
-
-
-
-
-
-
+		/*------------------------ Restaurante--------------------------*/
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-22.0f, 28.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Restaurante.RenderModel();
 
 
 
