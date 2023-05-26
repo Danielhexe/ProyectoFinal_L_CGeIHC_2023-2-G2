611f896df7ca66a9e16007a4238babb7000fa819
DanielHexe <CABALLERO.JUAN.3B@gmail.com>
2023-05-26 11:38:10 -0600

    El modelo del restaurante da problemas

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -322,8 +322,8 @@ int main()
 	Kubikiribocho.LoadModel("Models/Kubikiribocho.obj");
 	Shuriken4Dagas = Model();
 	Shuriken4Dagas.LoadModel("Models/Shuriken4Dagas.obj");
-	Restaurante = Model();
-	Restaurante.LoadModel("Models/RestauranteCompleto.obj");
+	//Restaurante = Model();
+	//Restaurante.LoadModel("Models/RestauranteCompleto.obj");
 
 
 	/* Variable a utilizar */
@@ -562,11 +562,12 @@ int main()
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Shuriken4Dagas.RenderModel();
 		/*------------------------ Restaurante--------------------------*/
+		/*
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-22.0f, 28.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		Restaurante.RenderModel();
+		Restaurante.RenderModel();*/
 
 
 
